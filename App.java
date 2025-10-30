cac4099af3c9aa989e58a41f20889f6deca7b0b7
Anay Choubey <anaychoubey@suttongrammar.school>
2025-10-30 07:33:25 +0000

    Update App.java

--- App.java
+++ App.java
@@ -19,7 +19,6 @@ public class App{
 
         Pacman pacmanGame = new Pacman();
         frame.add(pacmanGame);
-        frame.pack();
         pacmanGame.requestFocus();
         frame.setVisible(true);
   }  
